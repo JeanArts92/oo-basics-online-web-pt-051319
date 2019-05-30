43cdbc5742f1af46c2cc953daa7d2c7c9df445cc
Reginald Jean Amedee <reginald.amedee@gmail.com>
2019-05-30 00:54:47 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -11,8 +11,9 @@ class Shoe
   def cobble(condition)
    if condition == "old"
      puts "Your shoe is as good as new!"
+     condition = "new"
   end 
-  condition = "new"
+  condition 
 end
   
 
